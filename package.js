92a1968a7528f8bac04af56d973ce0a4beffef72
Daniel Li <dan@danyll.com>
2015-01-08 01:04:17 +0800

    Changed version number to 0.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'd4nyll:epic',
   summary: 'A JavaScript Markdown editor for Meteor, using EpicEditor.',
-  version: '0.0.0',
+  version: '0.0.1',
   git: 'https://github.com/d4nyll/epic.git'
 });
 
