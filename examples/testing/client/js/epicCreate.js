a466761c8ad7da9206b1ce023f0eb5be548bc6e8
Daniel Li <dan@danyll.com>
2015-02-04 09:46:58 +0800

    Fixed testing example

--- examples/testing/client/js/epicCreate.js
+++ examples/testing/client/js/epicCreate.js
@@ -1,16 +1,10 @@
 Template.epicCreate.rendered = function () {
-	var opts = {
-
-		/* In this test directory, 
-		** the `bathPath` paramter will have no effect,
-		** as it will always be overriden (as designed),
-		** and so this custom theme will not show in this test application.
-		** However, if you download and install the package normally, it will work.
-		*/
-	
-		basePath: '',
+	var opts = {	
+		basePath: '/epic/themes',
 		theme: {
-			editor: '/epic/themes/editor/epic-custom.css'
+			base: '/base/epiceditor.css',
+			preview: '/preview/github.css',
+			editor: '/editor/epic-custom.css'
 		}
 	};
 	Epic.create('epicCreateContainer', opts);
