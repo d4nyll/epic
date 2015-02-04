353339064aaa439de2fb64640f14cf4b0e72851b
Daniel Li <dan@danyll.com>
2015-02-04 09:30:28 +0800

    Allow user to override default settings when using API

--- d4nyll:epic.js
+++ d4nyll:epic.js
@@ -29,7 +29,9 @@ var MergeRecursive = function (obj1, obj2) {
 			if (obj2[p].constructor == Object) {
 				obj1[p] = MergeRecursive(obj1[p], obj2[p]);
 			} else {
-				obj1[p] = obj2[p];
+				if(typeof obj1[p] === 'undefined') {
+					obj1[p] = obj2[p];
+				}
 			}
 		} catch(e) {
 			obj1[p] = obj2[p];
