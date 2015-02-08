cc41416610fbf7850fcf0f2e3a06877e65a178e2
Daniel Li <dan@danyll.com>
2015-02-09 00:00:13 +0800

    Added examples for the 'Preload Text' feature

---
+++ examples/testing/client/js/preloadapi.js
@@ -0,0 +1,6 @@
+Template.preloadapi.rendered = function () {
+	var opts = {
+		preloadText: 'This is preloaded text using the API!\nCan even do new lines!'
+	};
+	Epic.create('preloadapicontainer', opts);
+};
--- examples/testing/client/js/router.js
+++ examples/testing/client/js/router.js
@@ -9,3 +9,11 @@ Router.route('/epicCreate', function () {
 Router.route('/epicMultiple', function () {
   this.render('epicMultiple');
 });
+
+Router.route('/preload', function () {
+  this.render('preload');
+});
+
+Router.route('/preloadapi', function () {
+  this.render('preloadapi');
+});
